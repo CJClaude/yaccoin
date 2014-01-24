5d970b15054c17239b5359cfc25dc552b8dcf7f6
Cassey Jean Claude <cassey.jean.claude@gmail.com>
2014-01-24 09:23:59 -0600

    Initial Litecoin->YACcoin changes

--- src/protocol.h
+++ src/protocol.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
+/// Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 19333 : 9333;
+    return testnet ? 16333 : 6333;
 }
 
 
