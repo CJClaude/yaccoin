bd550df0b42067720904a9c727ee87be78d811b6
Cassey Jean Claude <cassey.jean.claude@gmail.com>
2014-01-29 13:26:12 -0600

    QT work

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("YACCoins");
+    case mBTC: return QString("Milli-YACCoins (1 / 1,000)");
+    case uBTC: return QString("Micro-YACCoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
