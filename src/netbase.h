0d10f90de1c89c110c1924add9d0abdf0faeb50f
Cassey Jean Claude <cassey.jean.claude@gmail.com>
2014-01-31 14:26:49 -0600

    Still fussing with Windows-QT build

--- src/netbase.h
+++ src/netbase.h
@@ -15,6 +15,7 @@ extern int nConnectTimeout;
 #ifdef WIN32
 // In MSVC, this is defined as a macro, undefine it to prevent a compile and link error
 #undef SetPort
+#include "winsock2.h"
 #endif
 
 enum Network
