eb46e886b0f0ef4a7e680ba7137130115dfa4fe4
CasseyJeanClaude <cassey.jean.claude@gmail.com>
2014-02-11 15:11:16 -0600

    just cleanup

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("YACC");
+    case mBTC: return QString("mYACC");
+    case uBTC: return QString::fromUtf8("μYACC");
     default: return QString("???");
     }
 }
